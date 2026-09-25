29ad54c4d7e421e7e7ff4f93ebfb1cd1566dc494
THIYAGA SRI CHARUMATHY <159044760+Sricharumathy@users.noreply.github.com>
2026-09-25 10:25:38 +0530

    Update UnBoundedQueue.sv

--- Arrays/UnBoundedQueue.sv
+++ Arrays/UnBoundedQueue.sv
@@ -4,11 +4,12 @@ module Unboundedqtb;
     array='{0,2,4,6,8};
     $display("%p",array);
     array.push_back(10);
-    $display("%p",array);
-    $display("%p",array.pop_back);
+    $display("push the element in the back %p",array);
+    $display("popping the last element %p",array.pop_front);
+    $display("After pop elements in the array  %p",array);
+    $display("popping the first element %p",array.pop_back);
     array.push_front(1);
-    $display("%p",array);
-    
+    $display(" push the element in the front %p",array);
   end
 endmodule
     
